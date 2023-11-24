3da06cbf38eabcbc1c8e8873859a6f0abb91c853
YutingLi2001 <2001yuting.li@gmail.com>
2023-11-23 19:51:40 -0500

    made changes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,32 +1,23 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# The following admin classes are used to customize the Django admin interface for our models.
+
+# Register your models here.
 
 class CarModelInline(admin.StackedInline):
-    """
-    This inline class allows us to edit `CarModel` objects directly from the `CarMake` admin page.
-    """
     model = CarModel
-    extra = 1
+    extra = 6
 
 class CarModelAdmin(admin.ModelAdmin):
-    """
-    This class provides custom configurations for the `CarModel` admin interface.
-    """
-    list_display = ('name', 'dealer_id', 'car_type', 'year')  # Use tuples instead of lists for immutability
-    search_fields = ('name',)  # Even if there's only one field, make it a tuple for consistency
+    list_display = ['name',  'dealer_id', 'car_type', 'year']
+    search_fields = ['name']
 
 class CarMakeAdmin(admin.ModelAdmin):
-    """
-    This class provides custom configurations for the `CarMake` admin interface.
-    """
-    inlines = [CarModelInline]  # Allows editing of related `CarModel` objects on the same page
-    list_display = ('name', 'description')  # Display these fields in the list view
-    search_fields = ('name', 'description')  # Allow searching by name and description
+    inlines = [CarModelInline]
+    list_display = ['name', 'description']
+    search_fields = ['name']
 
-# The `admin.site.register` function is used to register the above configurations with the admin site.
+# Register models here
 
 admin.site.register(CarModel, CarModelAdmin)
-admin.site.register(CarMake, CarMakeAdmin)  # We should also pass `CarMakeAdmin` to use its configurations
-
+admin.site.register(CarMake)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,83 +1,78 @@
 from django.db import models
 from django.utils.timezone import now
 
-# Using Python's enum library to define choices in a more robust way
-from enum import Enum
 
-class CarType(Enum):
+# Create your models here.
+
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=50)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
+
+
+class CarModel(models.Model):
     SEDAN = 'sedan'
     SUV = 'suv'
     WAGON = 'wagon'
-
-    @classmethod
-    def choices(cls):
-        # This method will turn the enum into a tuple list ready for Django's choices
-        return [(key.value, key.name.title()) for key in cls]
-
-# Each model class will have a docstring explaining the purpose of the model
-class CarMake(models.Model):
-    """
-    Car Make represents the brand of the car.
-    """
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=1000)  # Use TextField for long strings
-
-    def __str__(self):
-        # Improved readability using f-strings
-        return f"Name: {self.name}, Description: {self.description}"
-
-class CarModel(models.Model):
-    """
-    Car Model represents a specific model of a car which belongs to a car make.
-    """
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'Suv'),
+        (WAGON, 'Wagon')
+    ]
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    dealer_id = models.IntegerField()  # Assuming this refers to an external ID
-    car_type = models.CharField(
-        max_length=50,
-        choices=CarType.choices(),  # Using the choices method from the CarType enum
-    )
+    name = models.CharField(null=False, max_length=50)
+    dealer_id = models.IntegerField()
+    car_type = models.CharField(max_length=50, choices=CAR_TYPES)
     year = models.DateField()
 
     def __str__(self):
-        # Using f-strings and accessing the year directly from the date object
-        return f"Name: {self.name}, Dealer ID: {self.dealer_id}, Type: {self.car_type}, Year: {self.year.year}"
+        return "Name: " + self.name + "," + \
+                "Dealer ID: " + str(self.dealer_id) + "," + \
+               "Type: " + self.car_type + "," + \
+               "Year: " + str(self.year.year)
 
-# The following classes are not Django models, but if they should be, they need to inherit from models.Model
+
 class CarDealer:
-    """
-    Car Dealer represents the information of a car dealership.
-    """
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
-        # Using descriptive parameter names and adding comments where necessary
+        # Dealer address
         self.address = address
+        # Dealer city
         self.city = city
+        # Dealer Full Name
         self.full_name = full_name
+        # Dealer id
         self.id = id
+        # Location lat
         self.lat = lat
+        # Location long
         self.long = long
+        # Dealer short name
         self.short_name = short_name
+        # Dealer state
         self.st = st
+        # Dealer zip
         self.zip = zip
 
     def __str__(self):
-        return f"Dealer name: {self.full_name}"
+        return "Dealer name: " + self.full_name
+
 
 class DealerReview:
-    """
-    Dealer Review contains customer reviews for a dealership, including sentiment analysis.
-    """
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
-        self.dealership = dealership
-        self.name = name
-        self.purchase = purchase
-        self.review = review
-        self.purchase_date = purchase_date
-        self.car_make = car_make
-        self.car_model = car_model
-        self.car_year = car_year
-        self.sentiment = sentiment  # Presumed to be calculated via Watson NLU service
-        self.id = id
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year,sentiment, id):
+        self.dealership=dealership
+        self.name=name
+        self.purchase=purchase
+        self.review=review
+        self.purchase_date=purchase_date
+        self.car_make=car_make
+        self.car_model=car_model
+        self.car_year=car_year
+        self.sentiment=sentiment #Watson NLU service
+        self.id=id
 
     def __str__(self):
-        return f"Review: {self.review}, Sentiment: {self.sentiment}"
+        return "Review: " + self.review +\
+                " Sentiment: " + self.sentiment
