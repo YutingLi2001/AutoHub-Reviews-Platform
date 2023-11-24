07809f21bbe9ed2f3838fc18baa0d8daa82b59a6
YutingLi2001 <2001yuting.li@gmail.com>
2023-11-23 20:24:16 -0500

    refactored models.py & admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,23 +1,31 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
-# Register your models here.
-
 class CarModelInline(admin.StackedInline):
+    """
+    CarModelInline allows for direct editing of CarModel instances within CarMake's admin page.
+    """
     model = CarModel
-    extra = 6
+    extra = 1  # Reducing the number of extra forms to a reasonable default
 
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ['name',  'dealer_id', 'car_type', 'year']
-    search_fields = ['name']
+    """
+    CarModelAdmin defines the admin interface for CarModel.
+    """
+    list_display = ('name', 'dealer_id', 'car_type', 'year')  # Use tuples instead of lists for immutability
+    list_filter = ('car_type', 'year')  # Quick filter options
+    search_fields = ('name', 'dealer_id')  # Allow searching by dealer ID as well
+    raw_id_fields = ('make',)  # Use raw_id_fields for ForeignKey to improve performance
 
 class CarMakeAdmin(admin.ModelAdmin):
+    """
+    CarMakeAdmin defines the admin interface for CarMake.
+    """
     inlines = [CarModelInline]
-    list_display = ['name', 'description']
-    search_fields = ['name']
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    # Optionally, add list_filter here if CarMake has fields that would benefit from filtering
 
-# Register models here
-
+# Register models with their respective ModelAdmin
 admin.site.register(CarModel, CarModelAdmin)
-admin.site.register(CarMake)
+admin.site.register(CarMake, CarMakeAdmin)  # Register CarMake with CarMakeAdmin to utilize the admin configuration
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,78 +1,76 @@
 from django.db import models
 from django.utils.timezone import now
 
-
-# Create your models here.
-
 class CarMake(models.Model):
-    name = models.CharField(null=False, max_length=50)
-    description = models.CharField(max_length=1000)
+    """
+    Car Make represents a brand or manufacturer of cars.
+    """
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField(max_length=1000, blank=True)
 
     def __str__(self):
-        return "Name: " + self.name + "," + \
-               "Description: " + self.description
-
+        return "Name: {}, Description: {}".format(self.name, self.description)
 
 class CarModel(models.Model):
+    """
+    Car Model represents a specific model of car, which is made by a Car Make.
+    """
     SEDAN = 'sedan'
     SUV = 'suv'
     WAGON = 'wagon'
     CAR_TYPES = [
         (SEDAN, 'Sedan'),
-        (SUV, 'Suv'),
-        (WAGON, 'Wagon')
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
     ]
+
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(null=False, max_length=50)
+    name = models.CharField(max_length=50, blank=False)
     dealer_id = models.IntegerField()
     car_type = models.CharField(max_length=50, choices=CAR_TYPES)
     year = models.DateField()
 
     def __str__(self):
-        return "Name: " + self.name + "," + \
-                "Dealer ID: " + str(self.dealer_id) + "," + \
-               "Type: " + self.car_type + "," + \
-               "Year: " + str(self.year.year)
+        return "Name: {}, Dealer ID: {}, Type: {}, Year: {}".format(
+            self.name, self.dealer_id, self.car_type, self.year.year)
 
-
+# Consider converting these to Django models if they need to be stored in the database.
 class CarDealer:
+    """
+    Car Dealer represents a car dealership, including its location and contact info.
+    """
+    # Initialization method with attributes defined
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
-        # Dealer address
+        # Attributes with simple comments
         self.address = address
-        # Dealer city
         self.city = city
-        # Dealer Full Name
         self.full_name = full_name
-        # Dealer id
         self.id = id
-        # Location lat
         self.lat = lat
-        # Location long
         self.long = long
-        # Dealer short name
         self.short_name = short_name
-        # Dealer state
         self.st = st
-        # Dealer zip
         self.zip = zip
 
     def __str__(self):
-        return "Dealer name: " + self.full_name
-
+        return "Dealer name: {}".format(self.full_name)
 
 class DealerReview:
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year,sentiment, id):
-        self.dealership=dealership
-        self.name=name
-        self.purchase=purchase
-        self.review=review
-        self.purchase_date=purchase_date
-        self.car_make=car_make
-        self.car_model=car_model
-        self.car_year=car_year
-        self.sentiment=sentiment #Watson NLU service
-        self.id=id
+    """
+    Dealer Review represents a customer's review of a car dealership.
+    """
+    # Initialization method with attributes defined
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment # Presumed to be provided by Watson NLU service
+        self.id = id
 
     def __str__(self):
-        return "Review: " + self.review +\
-                " Sentiment: " + self.sentiment
+        return "Review: {}, Sentiment: {}".format(self.review, self.sentiment)
